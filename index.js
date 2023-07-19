a8dceb199b7dfae41bb10988d0dcd35500ebbf30
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-19 17:13:48 -0500

    palindrome

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const length = word.length
+  let halfLength
+  let palindromeTruth = true
+  if(length%2===0){
+    halfLength = length/2 + 1
+  } else {
+    halfLength = length/2 + 0.5
+  }
+  for(let i = 0; i<halfLength; i++){
+    if(word.charAt(i)!==word.charAt(length-i-1)){
+      console.log(word.charAt(i)+word.charAt(length-i-1))
+      palindromeTruth = false
+    }
+  }
+  console.log(word)
+  return palindromeTruth
 }
 
 /* 
   Add your pseudocode here
+  Use charAt to find the first letter and last letter. 
+  Check if equal.
+  Keep trying until the counter equals the length/2 rounded up. 
 */
 
 /*
@@ -13,13 +31,23 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("racebar"));
+ 
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
+ 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("roob"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("roor"));
 }
 
 module.exports = isPalindrome;
